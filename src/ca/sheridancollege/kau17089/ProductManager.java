1dfb6e0988f2cee6c850861a6c66066b09c17094
Komalpreet-143 <kau17089@sheridancollege.ca>
2026-02-18 13:58:34 -0500

    Initial commit by Komalpreet Kaur

---
+++ src/ca/sheridancollege/kau17089/ProductManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.kau17089;
+
+/**
+ *
+ * @author komal
+ */
+public class ProductManager {
+    
+    public ProductManager() {
+        System.out.println("Product Manager created.");
+    }
+}
